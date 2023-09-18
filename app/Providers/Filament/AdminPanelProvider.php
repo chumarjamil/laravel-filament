8de6da7a49d8fe0a4e137f81c1479d2249a29a8f
chumarjamil <imumarj@gmail.com>
2023-09-18 16:44:47 +0700

    Change Logo, Font and Disable dark mode

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,10 @@ class AdminPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#674CC4',
             ])
+            ->font('Poppins')
+            ->darkMode(false)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,3 @@
+<h2
+    style="font-size: 20px;font-weight: 600;">
+    HRM</h2>
